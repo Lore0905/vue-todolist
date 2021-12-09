1c18afab98df1e1f8d575a39fa791891327efd61
Lore0905 <lore.castelli4@gmail.com>
2021-12-09 17:26:07 +0100

    MILESTONE 2

---
+++ MILESTONE 2/js/script.js
@@ -0,0 +1,30 @@
+Vue.config.devtools = true;
+
+var app = new Vue({
+    el: '#root',
+    data: {
+        todo: [
+            {
+                text: 'fare la spesa',
+                done: false
+            },
+            {
+                text: 'ritirare pacco Amazon',
+                done: true
+            },
+            {
+                text: 'cena ore: 20.00',
+                done: false
+            },
+            {
+                text: 'fare i compiti',
+                done: false
+            },
+        ],
+    },
+    methods: {
+        deleteTodo: function(index){
+            this.todo.splice(index, 1)
+        }
+    }
+})
