5ba49853af168e9fc1a3604df09c0914d8cef59f
Lore0905 <lore.castelli4@gmail.com>
2021-12-09 16:45:01 +0100

    milestone 1 done

--- MILESTONE 1/js/script.js
+++ MILESTONE 1/js/script.js
@@ -3,7 +3,24 @@ Vue.config.devtools = true;
 var app = new Vue({
     el: '#root',
     data: {
-        
+        todo: [
+            {
+                text: 'fare la spesa',
+                done: false
+            },
+            {
+                text: 'ritirare pacco Amazon',
+                done: true
+            },
+            {
+                text: 'cena ore: 20.00',
+                done: false
+            },
+            {
+                text: 'fare i compiti',
+                done: false
+            },
+        ],
     },
     methods: {
 
